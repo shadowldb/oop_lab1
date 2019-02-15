d631f6cd8ccd4dab6e0a0daa6dd181c0c05a3ad7
Mohamed Ali Larbi Daho Bachir <alildb2036@gmail.com>
2019-02-15 09:38:08 -0300

    Update Inventory.cpp

--- Inventory.cpp
+++ Inventory.cpp
@@ -11,19 +11,49 @@
 
 void InventoryGettingData(ifstream& fin, inventoryStructType& inventory1, int& invetoryCount) {
     
-    
+	///We need to put everything in loop until the end of the file
+	inventory1.totalOnHandQuantity = 0;
+
     getline(fin, inventory1.productName);
     getline(fin, inventory1.productNumber);
-    fin.ignore(1000, '\n');
-    //Store onHand into Array
-    for (int i = 0; i < 3; i++) {
+
+    //Store onHand into Array  (nested loop) & Calculate total Quantity
+    for (int i = 0; i <= 2; i++) {
         invetoryCount = 0;
-        for (int j = 0; j < 4; j++) {
-            fin >> inventory1.onHand[i][j] ;
-                invetoryCount +=1;
+        for (int j = 0; j <= 3; j++) {
+
+			fin >> inventory1.onHand[i][j];
+			inventory1.totalOnHandQuantity = inventory1.totalOnHandQuantity + inventory1.onHand[i][j];
+            invetoryCount +=1;
             
         }
     }
-    fin.ignore(1000, '\n');
     fin >> inventory1.minimumOnHandQuantity >> inventory1.cost;
+
+	//Getting value of Reorder Flag
+	inventory1.reorderFlag = ReorderFlag(inventory1.totalOnHandQuantity, inventory1.minimumOnHandQuantity);
+
+	///Formula to calculate Price
+	/*///
+		* Will be constant 175/100 = 1.75 
+		* then 1.75 * cost = > 1.75 * 7.55 ===> Markup Amount
+		* Markup amount + cost = Price ==> ex. 13.21 + 7.55 = 20.76
+		
+	*////
+	//Calculate Price
+	inventory1.price = MARKUP  * inventory1.cost;
+	inventory1.price = inventory1.price + +inventory1.cost;
+
+}
+
+//reorder flag
+bool ReorderFlag(int quantity, int minOnHand) {
+
+	bool reorderFlag = false;
+
+	if (quantity < minOnHand) {
+		reorderFlag = true;
+	}
+
+	return reorderFlag;
 }
