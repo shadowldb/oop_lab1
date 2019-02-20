38f725d129ebed6ad699594980ac6596f2bc8aef
Mohamed Ali Larbi Daho Bachir <alildb2036@gmail.com>
2019-02-20 12:09:08 -0500

    Declaring some functions
    Declaring some functions on Order Processing header file

--- OrderProcessing.h
+++ OrderProcessing.h
@@ -1,1 +1,29 @@
-#pragma once
+//---------------------------------------------------------------------------------------------------------------------
+//Programmer's Name:  Mohamed Ali Larbi Daho Bachir
+//File Name:          Company.h
+//Associated File:    None
+//Header File contains:
+// - Struct Type for Inventory
+
+//---------------------------------------------------------------------------------------------------------------------
+
+#ifndef OrderProcessing_h
+#define OrderProcessing_h
+#include"Standards.h"
+#include"Company.h"
+#include"Inventory.h"
+
+
+//Check and look for CompanyID in CompanyStructType && ProcessInput.txt
+int CompanyIDChecker(ifstream& fin, int companyCount, companyStructType companyO[], int processCount);
+
+//Product Number ID from Inventory
+int ProductNumberIDo(ifstream& fin, int inventoryCounts, string& productNid, inventoryStructType inventoryO);
+ 
+//Output Data into Lab1Out.txt
+void PrintProcessingOrder(ofstream& fout, companyStructType company[], inventoryStructType inventory[], int companyCount, int inventoryCount, int indexCompany);
+
+
+
+#endif // !OrderProcessing_h
+
