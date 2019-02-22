6dae65a8fd34825241c18da0cc06fad73114b2d2
Mohamed Ali Larbi Daho Bachir <alildb2036@gmail.com>
2019-02-22 16:01:42 -0500

    Update OrderProcessing.h

--- OrderProcessing.h
+++ OrderProcessing.h
@@ -3,7 +3,7 @@
 //File Name:          Company.h
 //Associated File:    OrderProcessing.cpp
 //Header File contains:
-// Header File contains: None
+// Header File contains: processingOrderStruct
 //Prototype for:
 //CompanyIDChecker, ProductNumberIDo, PrintProcessingOrder
 
@@ -15,18 +15,36 @@
 #include"Company.h"
 #include"Inventory.h"
 
+//Creating struct to store ProcessInput.txt file with Company and Inventory Location match. 
+struct processingOrderStruct 
+{
+    string orderNumber;
+    string orderProductNumber;
+    
+    int orderCompanyID;
+    int quantityOrder;
+    int companyLocation;
+    int inventoryLocation;
+    
+};
+
+//Store ProcessInput.txt into Struct
+void StoreDataIntoStruct(ifstream& fin, processingOrderStruct orderStruct[], int& processingCounter);
 
 //Check and look for CompanyID in CompanyStructType && ProcessInput.txt
-int CompanyIDChecker(ifstream& fin, int companyCount, companyStructType companyO[], int& processCount);
+int CompanyIDChecker(int companyCount, companyStructType companyO[], int companyIdProcess, int& processCount);
 
 //Product Number ID from Inventory
-int ProductNumberIDo(ifstream& fin, int inventoryCounts, string& productNid, inventoryStructType inventoryO[], int processCount);
+int ProductNumberIDos(string productIDprocess, int inventoryCount, inventoryStructType inventoryO[]);
 
 //Update Inventory if needed
 void UpdateInventory(ifstream& fin, inventoryStructType inventoryO[], int inventoryCounter);
 
 //Output Data into Lab1Out.txt
 void PrintProcessingOrder(ofstream& fout, companyStructType company[], inventoryStructType inventory[], int companyCount, int inventoryCount, int indexCompany);
+
+//Function to print Centered Name
+void PrintCenteredMessage(ofstream& fout, string titleOrder, int width);
 
 
 
